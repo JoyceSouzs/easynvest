a70368c7a9eeb3750e9d3db191de9b4c64a48ba0
Joyce Souza <joyce.souza@mesainc.com.br>
2020-08-28 20:26:05 -0300

    fix(simulatorCalulator): ids do layout

--- app/src/main/java/com/example/easynvesttest/providers/repository/SimulatorCalculatorRepositoryImpl.kt
+++ app/src/main/java/com/example/easynvesttest/providers/repository/SimulatorCalculatorRepositoryImpl.kt
@@ -14,11 +14,35 @@ class SimulatorCalculatorRepositoryImpl(
         parametersRequest: ParametersRequest
     ): SimulatorCalculatorData {
 
-        val response = dataSource.getInvestmentValues(parametersRequest)
-        if (response.isSuccessful) {
-            return response.body()!!.toInvestment()
-        } else {
-            throw SimulatorCalcApiException(response.code())
-        }
+//        val response = dataSource.getInvestmentValues(parametersRequest)
+//        if (response.isSuccessful) {
+//            return response.body()!!.toInvestment()
+//        } else {
+//            throw SimulatorCalcApiException(response.code())
+//        }
+
+        val SIMULATION = SimulatorCalculatorData(
+            investmentParameter = InvestmentParameter(
+                investedAmount = 32323.0,
+                yearlyInterestRate = 9.5512,
+                maturityTotalDays = 1981,
+                maturityBusinessDays = 1409,
+                maturityDate = LocalDateTime.of(2023, 3, 3, 0, 0),
+                rate = 123.0,
+                isTaxFree = false,
+            ),
+            grossAmount = 60528.20,
+            taxesAmount = 4230.78,
+            netAmount = 56297.42,
+            grossAmountProfit = 28205.20,
+            netAmountProfit = 23974.42,
+            annualGrossRateProfit = 87.26,
+            monthlyGrossRateProfit = 0.76,
+            dailyGrossRateProfit = 0.000445330025305748,
+            taxesRate = 15.0,
+            rateProfit = 9.5512,
+            annualNetRateProfit = 74.17
+        )
+        return SIMULATION
     }
 }
