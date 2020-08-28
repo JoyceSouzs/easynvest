de1fbb0115191436ea0481684ea1314cdef00a66
Joyce Souza <joyce.souza@mesainc.com.br>
2020-08-28 11:21:23 -0300

    feat(simulatorCalulator): teste unitário view model

---
+++ app/src/test/java/com/example/easynvesttest/data/TestSimulatorCalculatorData.kt
@@ -0,0 +1,35 @@
+package com.example.easynvesttest.data
+
+import com.example.easynvesttest.presentation.model.InvestmentParameter
+import com.example.easynvesttest.presentation.model.SimulatorCalculatorData
+import org.threeten.bp.LocalDateTime
+
+object TestSimulatorCalculatorData {
+
+    private val investmentParameter = InvestmentParameter(
+        investedAmount = 32323.0,
+        yearlyInterestRate = 9.5512,
+        maturityTotalDays = 1981,
+        maturityBusinessDays = 1409,
+        maturityDate = LocalDateTime.of(2023, 3, 3, 0, 0),
+        rate = 123.0,
+        isTaxFree = false,
+    )
+
+    val simulator_data = SimulatorCalculatorData(
+        investmentParameter = investmentParameter,
+        grossAmount = 60528.20,
+        taxesAmount = 4230.78,
+        netAmount = 56297.42,
+        grossAmountProfit = 28205.20,
+        netAmountProfit = 23974.42,
+        annualGrossRateProfit = 87.26,
+        monthlyGrossRateProfit = 0.76,
+        dailyGrossRateProfit = 0.000445330025305748,
+        taxesRate = 15.0,
+        rateProfit = 9.5512,
+        annualNetRateProfit = 74.17
+    )
+
+
+}
---
+++ app/src/test/java/com/example/easynvesttest/presentation/features/viewmodel/SimulatorCalculatorViewModelTest.kt
@@ -0,0 +1,80 @@
+package com.example.easynvesttest.presentation.features.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.easynvesttest.data.TestSimulatorCalculatorData.simulator_data
+import com.example.easynvesttest.domain.request.ParametersRequest
+import com.example.easynvesttest.presentation.model.SimulatorCalculatorData
+import com.example.easynvesttest.providers.repository.SimulatorCalculatorRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.threeten.bp.LocalDateTime
+
+@ExperimentalCoroutinesApi
+class SimulatorCalculatorViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+    private val viewModel = SimulatorCalculatorViewModel(FakeSimulatorCalculatorRepository())
+
+    @Test
+    fun `when view model calculatorInvestment try sets _simulatorCalculatorData`()
+            = testDispatcher.runBlockingTest {
+        //when
+        viewModel.calculatorInvestment()
+
+        //then
+        assertEquals(simulator_data, viewModel.simulatorCalculatorData.value)
+        assertEquals(false, viewModel.loadingLiveData.value)
+
+    }
+
+    @Test
+    fun `when view model setParameters sets parametersRequest`()
+            = testDispatcher.runBlockingTest {
+        //given
+        val parameters = ParametersRequest(
+            investedAmount = 32323.0,
+            rate = 123.0,
+            maturityDate = "2023-03-03"
+        )
+
+        //when
+        viewModel.setParameters(parameters)
+
+        //then - Remover o private do view model para ter acesso
+//        assertEquals(parameters, viewModel.parameters)
+
+    }
+
+    internal class FakeSimulatorCalculatorRepository : SimulatorCalculatorRepository {
+
+        override suspend fun getInvestmentValues(parametersRequest: ParametersRequest)
+                : SimulatorCalculatorData {
+            return simulator_data
+        }
+    }
+
+}
