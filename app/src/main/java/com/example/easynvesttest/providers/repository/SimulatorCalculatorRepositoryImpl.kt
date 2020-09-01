e9ffd9b79293d299e419fb47f667dbb008f476f2
Joyce Souza <joyce.souza@mesainc.com.br>
2020-09-01 17:43:34 -0300

    fix(simulatorCalulator): refactor versões no gradle, remoção do mock

--- app/src/main/java/com/example/easynvesttest/providers/repository/SimulatorCalculatorRepositoryImpl.kt
+++ app/src/main/java/com/example/easynvesttest/providers/repository/SimulatorCalculatorRepositoryImpl.kt
@@ -14,42 +14,16 @@ class SimulatorCalculatorRepositoryImpl(
         parametersRequest: ParametersRequest
     ): SimulatorCalculatorResult<SimulatorCalculatorData> {
 
-//        return try {
-//            val response = dataSource.getInvestmentValues(parametersRequest)
-//            if (response.isSuccessful) {
-//                val data = response.body()!!.toInvestment()
-//                SimulatorCalculatorResult.Success(data)
-//            } else {
-//                SimulatorCalculatorResult.ApiError(response.code(), response.message())
-//            }
-//        } catch (throwable: Throwable) {
-//            SimulatorCalculatorResult.ApiError(0, throwable.message.toString())
-//        }
-
-
-
-        val simulation = SimulatorCalculatorData(
-            investmentParameter = InvestmentParameter(
-                investedAmount = 32323.0,
-                yearlyInterestRate = 9.5512,
-                maturityTotalDays = 1981,
-                maturityBusinessDays = 1409,
-                maturityDate = LocalDateTime.of(2023, 3, 3, 0, 0),
-                rate = 123.0,
-                isTaxFree = false,
-            ),
-            grossAmount = 60528.20,
-            taxesAmount = 4230.78,
-            netAmount = 56297.42,
-            grossAmountProfit = 28205.20,
-            netAmountProfit = 23974.42,
-            annualGrossRateProfit = 87.26,
-            monthlyGrossRateProfit = 0.76,
-            dailyGrossRateProfit = 0.000445330025305748,
-            taxesRate = 15.0,
-            rateProfit = 9.5512,
-            annualNetRateProfit = 74.17
-        )
-        return SimulatorCalculatorResult.Success(simulation)
+        return try {
+            val response = dataSource.getInvestmentValues(parametersRequest)
+            if (response.isSuccessful) {
+                val data = response.body()!!.toInvestment()
+                SimulatorCalculatorResult.Success(data)
+            } else {
+                SimulatorCalculatorResult.ApiError(response.code(), response.message())
+            }
+        } catch (throwable: Throwable) {
+            SimulatorCalculatorResult.ApiError(0, throwable.message.toString())
+        }
     }
 }
